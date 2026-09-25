5eb3ac7f567884eaacf9af95d26432ef38559ff0
Shivani <shivanisalunke27@gmail.com>
2026-09-25 16:15:24 +0530

    fix(migrations): chain alembic revisions sequentially into single head

--- server/alembic/versions/0003_add_event_fields.py
+++ server/alembic/versions/0003_add_event_fields.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '0003_add_event_fields'
-down_revision = '0002_add_events_table'
+down_revision = '0003_add_event_id_to_jobs'
 branch_labels = None
 depends_on = None
 
